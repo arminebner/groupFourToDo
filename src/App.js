36c1542a056c13699c38032cb343686678415288
Armin Ebner <armin_ebner@web.de>
2020-11-26 14:40:54 +0100

    moving up the state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
 import Welcome from './components/Welcome'
 import Overview from './components/Overview'
 import './App.css';
 
 
 function App() {
+
+   //State stuff
+   const [inputText, setInputText] = useState("");
+   const [todos, setTodos] = useState([]);
+   const [status, setStatus] = useState("all");
+   const [filteredTodos, setFilteredTodos] = useState([]);
+ 
+ 
+   //Run once when the app start
+   useEffect(() => {
+     getLocalTodos();
+   }, []);
+ 
+ 
+   //Use effect
+   useEffect(() => {
+     filterHandler();
+     saveLocalTodos();
+   }, [todos, status]);
+ 
+ 
+   //Functions
+   const filterHandler = () => {
+     switch(status){
+       case 'completed':
+         setFilteredTodos(todos.filter(todo => todo.completed === true));
+         break;
+       case 'uncompleted':
+         setFilteredTodos(todos.filter(todo => todo.completed === false));
+         break;
+       default:
+         setFilteredTodos(todos);
+         break;
+     }
+   };
+ 
+ 
+   //Save to local
+   const saveLocalTodos = () => {
+     if(localStorage.getItem('todos') === null){
+       localStorage.setItem('todos', JSON.stringify([]));
+     } else {
+       localStorage.setItem('todos', JSON.stringify(todos));
+     }
+   };
+   
+   const getLocalTodos = () => {
+     if(localStorage.getItem('todos') === null){
+       localStorage.setItem('todos', JSON.stringify([]));
+     } else {
+       let todoLocal = JSON.parse(localStorage.getItem("todos"));
+       setTodos(todoLocal);
+     }
+   };
 
   return (
     <>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import happy from '../images/happy.svg'
+
 
 import '../styles-welcome.css';
 
 
 const Welcome = () => {
+
+    let expression = 'Oh cool'
+    let counter = 0
+
+
     return (
         <>
             <div className="main-wrapper">
                 <div className="content">
                     <div className="headLine">Welcome back</div>
-                    <img url='{}' alt='welcome' />
+                    <p className="text">{`${expression}! You have ${counter} Tasks to do!`}</p>
+                    <img className='avatar' src={happy} alt='welcome' />
                     <div className="button-container">
-                        <NavLink to='/overview' >see overview</NavLink>
+                        <NavLink className='nav-link' to='/overview' >see overview</NavLink>
                     </div>
                 </div>
             </div>
